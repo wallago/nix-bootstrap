00237294d96a3939b748a51f6ad5869747757deb
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-04-24 03:15:01 +0200

    init

--- src/help.rs
+++ src/help.rs
@@ -1,11 +1,41 @@
-use std::io::{self, Write};
+use std::sync::Arc;
 
-pub fn ask_yes_no(prompt: &str) -> bool {
-    print!("{} [y/N]: ", prompt);
-    io::stdout().flush().unwrap(); // Ensure the prompt prints before input
+use anyhow::Result;
+use tempfile::{TempDir, tempdir};
+use tokio::{
+    io::{self, AsyncBufReadExt, AsyncWriteExt, BufReader},
+    sync::Mutex,
+};
+
+pub async fn ask_yes_no(prompt: &str) -> Result<bool> {
+    let mut stdout = io::stdout();
+    stdout
+        .write_all(format!("{} [y/N]: ", prompt).as_bytes())
+        .await?;
+    stdout.flush().await?;
 
     let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
+    let mut reader = BufReader::new(io::stdin());
+    reader.read_line(&mut input).await?;
 
-    matches!(input.trim().to_lowercase().as_str(), "y" | "yes")
+    Ok(matches!(input.trim().to_lowercase().as_str(), "y" | "yes"))
+}
+
+pub fn creat_tmp_dir() -> Result<TempDir> {
+    let temp_dir = tempdir()?;
+    println!(
+        "Temporary directory created at: {}",
+        temp_dir.path().display()
+    );
+    Ok(temp_dir)
+}
+
+pub async fn clear_tmp_dir(temp_dir: Arc<Mutex<Option<TempDir>>>) -> Result<()> {
+    if let Some(dir) = temp_dir.lock().await.take() {
+        dir.close()?;
+        println!("Temporary directory cleared");
+    } else {
+        println!("Temporary directory already cleared");
+    }
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+use std::sync::Arc;
+
+use anyhow::Result;
 use clap::Parser;
+use tokio::{signal, sync::Mutex};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -6,13 +10,28 @@ struct Args {}
 
 mod help;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<()> {
     let args = Args::parse();
-    println!("Hello, world!");
 
-    if help::ask_yes_no("Do you want to continue?") {
+    let temp_dir = Arc::new(Mutex::new(Some(help::creat_tmp_dir()?)));
+    let temp_dir_copy = temp_dir.clone();
+
+    tokio::spawn(async move {
+        signal::ctrl_c().await.expect("Failed to listen for Ctrl-C");
+        if let Err(e) = help::clear_tmp_dir(temp_dir_copy).await {
+            eprintln!("Failed to clear tmp dir: {e}");
+        }
+        std::process::exit(130);
+    });
+
+    if help::ask_yes_no("Do you want to continue.?").await? {
         println!("You chose yes.");
     } else {
-        println!("You chose no.");
+        println!("You chose no or cancelled.");
     }
+
+    help::clear_tmp_dir(temp_dir).await?;
+
+    Ok(())
 }
