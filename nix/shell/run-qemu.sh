647432f39f2dc062db6eeb7bd70360d8848d0f24
wallago <45556867+wallago@users.noreply.github.com>
2025-06-10 00:05:58 +0200

    chore(devShell): update to make clean workflow

--- nix/shell/run-qemu.sh
+++ nix/shell/run-qemu.sh
@@ -1,26 +1,15 @@
-with_iso="$1"
-boot_flag="-boot c"
-iso_flag=""
-network="-net nic -net user,hostfwd=tcp::10022-:2222"
-
 if [ ! -f ${diskImage} ]; then
   echo "Disk image '${diskImage}' not found. Run create-qemu-disk first."
   return 1
-fi
-
-if [ "$with_iso" = "--iso" ]; then
-  boot_flag="-boot once=d"
-  iso_flag="-cdrom ${nixosIso}"
-  network="-net nic -net user,hostfwd=tcp::10022-:22"
 fi
 
 qemu-system-x86_64 \
   -enable-kvm \
   -m 4096 \
   -cpu host \
-  $boot_flag \
-  $iso_flag \
-  $network \
+  -boot once=d \
+  -cdrom ${nixosIso} \
+  -nic user,hostfwd=tcp::10022-:22,hostfwd=tcp::12222-:2222 \
   -drive file=vm-disk.qcow2,format=qcow2 \
   -vga std \
   -usb -device usb-tablet
